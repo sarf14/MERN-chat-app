661f64e55a2f9bf11a1ea8233bde5affc18be293
sarf14 <shaikhsarfaraz1426@gmail.com>
2024-08-24 13:59:02 +0530

    deployment  error update

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import MessageContainer from "../../components/messages/MessageContainer";
-import Sidebar from "../../components/Sidebar/Sidebar";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
